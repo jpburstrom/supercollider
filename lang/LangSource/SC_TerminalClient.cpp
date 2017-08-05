79cb0167da9c2b03859c4fea6fe5f8958bfd2bfb
Giulio Moro <giuliomoro@yahoo.it>
2017-08-05 15:22:50 +0000

    Check error code passed to tick() and return if it is not 'success'. This seems to be the solution for sensestage/supercollider/#34 and supercollider/supercollider/#2144 , however I have no explanation as to why this issue only manifests itself when QT is disabled

--- lang/LangSource/SC_TerminalClient.cpp
+++ lang/LangSource/SC_TerminalClient.cpp
@@ -398,7 +398,11 @@ extern void ElapsedTimeToChrono(double elapsed, std::chrono::system_clock::time_
 
 void SC_TerminalClient::tick( const boost::system::error_code& error )
 {
-	mTimer.cancel();
+	if (error == boost::system::errc::success) {
+		mTimer.cancel();
+	} else {
+		return;
+	}
 
 	double secs;
 	lock();
